b3f2bc350f830ac6c13ce4e96bc75fc45c669b4f
Shaurya Jha <shauryajha35@gmail.com>
2025-03-30 17:35:09 +0530

    [Add client changes]

--- apps/client/src/components/main/Navbar.tsx
+++ apps/client/src/components/main/Navbar.tsx
@@ -5,9 +5,15 @@ const Navbar = () => {
     <nav className="relative w-full bg-transparent text-white">
       <div className="max-w-screen-xl mx-auto flex items-center justify-between h-[103px] px-6">
         <div className="text-2xl md:text-3xl lg:text-4xl font-semibold leading-tight tracking-wide cursor-pointer">
-          Brixline
+        <Image
+            src="/svg/brixline-logo.svg"
+            alt="brixline-logo"
+            width={365}
+            height={94}
+            className="w-28 md:w-36 h-24"
+          />
         </div>
-        <button className="flex justify-center items-center border rounded-xl border-white py-2 px-3 lg:py-3 sm:px-6 gap-2.5 cursor-pointer">
+        <button className="flex justify-center items-center border rounded-xl border-white py-2 px-2 lg:py-3 sm:px-6 gap-2.5 cursor-pointer">
           <div className="text-md sm:text-lg">BUILD WITH US</div>
           <Image
             src="/svg/right-arrow.svg"
